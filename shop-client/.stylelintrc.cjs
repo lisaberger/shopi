3cdb441f5afdf083a802558ab7779fd9b8015cfc
bergerli <lisa.berger@hs-augsburg.de>
2023-11-13 19:21:02 +0100

    build(shop-ui): setup shop-client

---
+++ shop-client/.stylelintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['stylelint-config-standard'],
+    rules: {
+        'declaration-empty-line-before': 'never',
+        'property-no-vendor-prefix': null,
+        'color-function-notation': null,
+        'value-keyword-case': null,
+    },
+};
