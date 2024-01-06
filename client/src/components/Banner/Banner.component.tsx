3fc118f3c86872a882691049b31723c29c7d23ea
bergerli <lisa.berger@hs-augsburg.de>
2024-01-06 01:03:19 +0100

    feat: add product models

--- client/src/components/Banner/Banner.component.tsx
+++ client/src/components/Banner/Banner.component.tsx
@@ -14,9 +14,9 @@ const Banner: FC<BannerProps> = ({ onCloseBanner }) => {
         <div className={`${styles.banner} bg-primary-500 fixed w-full z-4`}>
             <div className='flex text-sm text-color py-2 px-4 md:px-8 flex flex-row justify-content-between align-items-center'>
                 <div></div>
-                <div className='flex'>
+                <div className='flex align-items-center'>
                     <p className='font-semibold'>🔥 Black Friday!</p>
-                    <p className='pl-2 pr-8 align-items-center hidden md:block'>Jetzt tolle Angebote sichern.</p>
+                    <p className='pl-2 pr-8 hidden md:block'>Jetzt tolle Angebote sichern.</p>
                 </div>
                 <i className='pl-2 px-2 pi pi-times cursor-pointer' onClick={onCloseHandler} />
             </div>
